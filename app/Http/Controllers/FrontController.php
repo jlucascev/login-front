0871c3a7f3602c634c40c599e95531d6f5516b74
Juan <juanrinconada@cev.com>
2019-12-12 14:24:17 +0100

    Envío cifrado JWT

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Firebase\JWT\JWT;
 
 class FrontController extends Controller
 {
@@ -11,16 +12,26 @@ class FrontController extends Controller
     	$viewParams = array("response" => "");
     	if ($req->isMethod('post')) {
 		    //
+
+    		$key = env("JWT_KEY");
+
+    		$payload = array(
+    			"email" => $req->email,
+    			"password" => $req->pass
+    		);
+
+    		$jwt = JWT::encode($payload, $key);
+
 		    $ch = curl_init(env('LOGIN_URL'));
 
 			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $req->email);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $jwt);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 			$response = curl_exec($ch);
 			curl_close($ch);
 
-			$viewParams['response'] = "Respuesta del servidor: ".$response;
+			$viewParams['response'] = "Respuesta del servidor: ".print_r($response,true);
 		}
     	return view('form',$viewParams);
     }
